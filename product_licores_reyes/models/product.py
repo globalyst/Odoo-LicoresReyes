d6dbb01903e9d4ff566f4950874018782aa407e6
globalyst <aparrac88@gmail.com>
2016-12-22 14:08:15 +0100

    Corregidos textos

--- product_licores_reyes/models/product.py
+++ product_licores_reyes/models/product.py
@@ -4,13 +4,13 @@ class Format(models.Model):
 	_name = 'product.format'
 	
 	name = fields.Char(string='Name')
-	description = fields.Char(string='Description')
-	capacity = fields.Integer(string='Capacity',default=0)
+	description = fields.Char(string='Descripcion')
+	capacity = fields.Integer(string='Capacidad',default=0)
 	is_default = fields.Boolean(default=True,string='Por defecto')
 	
 class product_template(models.Model):
 	_inherit = 'product.template'
 	
-	graduacion = fields.Float(digits=(4, 2), help="Alcoholic graduation")
+	graduacion = fields.Float(digits=(4, 2), string="Graduacion Alcoholica" help="Alcoholic graduation")
 	format_id = fields.Many2one('product.format',ondelete='cascade', string="Formato")
 
